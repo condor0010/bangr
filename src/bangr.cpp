02012b73e5f764a39b5dbcd5d75f2f7da3fd144f
Louie <liatmp3@gmail.com>
2024-09-09 10:33:22 -0400

    err

--- src/bangr.cpp
+++ src/bangr.cpp
@@ -8,21 +8,20 @@
 using namespace BinaryNinja;
 
 int main (int argc, char* argv[]) {
-	if (argc != 2){return 1;}
+	if(argc != 3){ printf("%s <binary> <address>\n", argv[0]); return 1;}
 
-	auto binary = argv[1];
-	auto addr   = argv[2]
+	auto binary_arg = argv[1];
+	auto addr_arg   = argv[2];
 
 	// plugin setup bs
-	SetBundledPluginDirectory(GetBundledPluginDirectory()); 
+	SetBundledPluginDirectory(GetBundledPluginDirectory());
 	InitPlugins();
 
 	// bv is same as in python
-	Ref<BinaryView> bv = BinaryNinja::Load(argv[1]);
+	Ref<BinaryView> bv = BinaryNinja::Load(binary_arg);
 
 	// for func in funcs:
 	for (auto& func : bv->GetAnalysisFunctionList()) {
 		printf("Function at 0x%" PRIx64 ":\n", func->GetStart());
 	}
 }
-
