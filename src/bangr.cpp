691e8b4c1867b5a1988e21f848f7fcb44db41da8
Louie <liatmp3@gmail.com>
2024-09-09 08:37:21 -0400

    mlil example in docs is nice, ripepd off some of what they did

--- src/bangr.cpp
+++ src/bangr.cpp
@@ -1,6 +1,26 @@
 #include<stdio.h>
-int main(){
-	printf("test\n");
-	return 0;
+#include <cinttypes>
+
+#include "binaryninjacore.h"
+#include "binaryninjaapi.h"
+#include "mediumlevelilinstruction.h"
+
+using namespace BinaryNinja;
+
+int main (int argc, char* argv[]) {
+	// exit if binary is not specified
+	if (argc != 2){return 1;}
+
+	// plugin setup bs
+	SetBundledPluginDirectory(GetBundledPluginDirectory()); 
+	InitPlugins();
+
+	// bv is same as in python
+	Ref<BinaryView> bv = BinaryNinja::Load(argv[1]);
+
+	// for func in funcs:
+	for (auto& func : bv->GetAnalysisFunctionList()) {
+		printf("Function at 0x%" PRIx64 ":\n", func->GetStart());
+	}
 }
 
