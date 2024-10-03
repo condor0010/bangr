1ec41035a9d4f1535e79f357c53a05eb04fb8710
Louie Orcinolo <liatmp3@gmail.com>
2024-10-02 22:49:51 -0400

    still just a ripoff of the MLIL example exept it bulds unlike the example

--- src/bangr.cpp
+++ src/bangr.cpp
@@ -1,38 +1,55 @@
+#include <vector>
 #include <stdio.h>
 #include <cinttypes>
-#include <typeinfo>
-#include <iostream>
-#include <vector>
 
-#include "binaryninjacore.h"
 #include "binaryninjaapi.h"
+#include "binaryninjacore.h"
 #include "mediumlevelilinstruction.h"
 
-using namespace BinaryNinja;
 using namespace std;
+using namespace BinaryNinja;
 
 int main(int argc, char* argv[]) {
-    if (argc != 3) {printf("%s <binary> <address>\n", argv[0]);}
+    if (argc != 3) {
+        printf("%s <binary> <address>\n", argv[0]);
+        return 1;
+    }
 
     char* binary_arg = argv[1];
-    //int addr_arg = 0x00401136;
     int addr_arg = atoi(argv[2]);
 
     SetBundledPluginDirectory(GetBundledPluginDirectory());
     InitPlugins();
 
+   
     Ref<BinaryView> bv = BinaryNinja::Load(binary_arg);
-    
+    if (!bv) {
+        printf("Failed to load binary: %s\n", binary_arg);
+        return 1;
+    }
+
+   
     std::vector<Ref<Function>> funcs = bv->GetAnalysisFunctionsForAddress(addr_arg);
-    
+    if (funcs.empty()) {
+        printf("No functions found at address: 0x%x\n", addr_arg);
+        return 1;
+    }
+
     Ref<Function> function = funcs[0];
-    
     Ref<MediumLevelILFunction> il = function->GetMediumLevelIL();
     
-    for (auto& block : il->GetBasicBlocks()){
-	    printf("%p", block);
+    printf("Analyzing function...\n");
+    for (auto& block : il->GetBasicBlocks()) {
+	// for each block in the desired fcn we will eventualy make workers to do the analisys and recombine later (that goes here)
+        printf("Basic block: %p\n", block);
+        for (size_t instrIndex = block->GetStart();instrIndex < block->GetEnd();instrIndex++) {
+            MediumLevelILInstruction instr = (*il)[instrIndex];
+            vector<InstructionTextToken> tokens;
+            il->GetInstructionText(function, function->GetArchitecture(), instrIndex, tokens);
+            printf("    %" PRIdPTR " @ 0x%" PRIx64 "\n", instrIndex, instr.address);
+        }
     }
-    
+
     return 0;
 }
 
