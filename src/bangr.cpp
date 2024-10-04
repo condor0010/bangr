461680abdd9285d96f9c5d9e1c9ba634c7ab49ed
Louie <liatmp3@gmail.com>
2024-10-04 15:02:51 -0400

    here ya go marcus

--- src/bangr.cpp
+++ src/bangr.cpp
@@ -21,14 +21,12 @@ int main(int argc, char* argv[]) {
     SetBundledPluginDirectory(GetBundledPluginDirectory());
     InitPlugins();
 
-   
     Ref<BinaryView> bv = BinaryNinja::Load(binary_arg);
     if (!bv) {
         printf("Failed to load binary: %s\n", binary_arg);
         return 1;
     }
 
-   
     std::vector<Ref<Function>> funcs = bv->GetAnalysisFunctionsForAddress(addr_arg);
     if (funcs.empty()) {
         printf("No functions found at address: 0x%x\n", addr_arg);
@@ -40,16 +38,27 @@ int main(int argc, char* argv[]) {
     
     printf("Analyzing function...\n");
     for (auto& block : il->GetBasicBlocks()) {
-	// for each block in the desired fcn we will eventualy make workers to do the analisys and recombine later (that goes here)
-        printf("Basic block: %p\n", block);
-        for (size_t instrIndex = block->GetStart();instrIndex < block->GetEnd();instrIndex++) {
+        printf("Basic block: %p\n", (void*)block->GetStart());
+        
+        for (size_t instrIndex = block->GetStart(); instrIndex < block->GetEnd(); instrIndex++) {
             MediumLevelILInstruction instr = (*il)[instrIndex];
             vector<InstructionTextToken> tokens;
+
             il->GetInstructionText(function, function->GetArchitecture(), instrIndex, tokens);
-            printf("    %" PRIdPTR " @ 0x%" PRIx64 "\n", instrIndex, instr.address);
+            printf("    Instruction @ 0x%" PRIx64 ": ", instr.address);
+
+            for (const auto& token : tokens) {
+                printf("%s ", token.text.c_str());
+            }
+            printf("\n");
+
+            printf("    SSA: ");
+            for (const auto& token : tokens) {
+                printf("%s ", token.text.c_str());
+            }
+            printf("\n");
         }
     }
-
     return 0;
 }
 
