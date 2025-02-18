72b42006140a7abc08c271ae401f006739054084
Louie Orcinolo <liatmp3@gmail.com>
2025-02-17 21:48:38 -0500

    UI code

--- ui/variable_list.py
+++ ui/variable_list.py
@@ -1,6 +1,6 @@
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QImage
-from binaryninja import BinaryView, PluginCommand
+from binaryninja import BinaryView, PluginCommand, HighlightStandardColor, HighlightColor
 from binaryninjaui import *
 from PySide6.QtWidgets import (
     QVBoxLayout, QTableWidget, QTableWidgetItem,
@@ -18,7 +18,6 @@ class VariableListWidget(SidebarWidget):
 
         # Regular variable table
         self.variableTable = self.createTable()
-        self.ssaVariableTable = self.createTable()
 
         # Wrap in QWidget for tab layout
         self.variableTab = QWidget()
@@ -30,8 +29,7 @@ class VariableListWidget(SidebarWidget):
         self.variableTab.setLayout(var_layout)
 
         ssa_layout = QVBoxLayout()
-        ssa_layout.addWidget(self.ssaVariableTable)
-        self.ssaTab.setLayout(ssa_layout)
+        self.ssaTab.setLayout(ssa_layout)  # Leave SSA tab empty
 
         # Add tabs
         self.tabWidget.addTab(self.variableTab, "Variables")
@@ -42,19 +40,21 @@ class VariableListWidget(SidebarWidget):
 
         if self.bv:
             self.populateVariableList()
+            self.bv.register_notification(self)
+
+        self.variableTable.itemSelectionChanged.connect(self.highlight_variable_usage)
 
     def createTable(self):
-        """ Creates a QTableWidget with two columns. """
+        """ Creates a QTableWidget with three columns. """
         table = QTableWidget()
-        table.setColumnCount(2)
-        table.setHorizontalHeaderLabels(["Variable", "Value"])
+        table.setColumnCount(3)
+        table.setHorizontalHeaderLabels(["Variable", "Value", "Location"])
         table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         return table
 
     def populateVariableList(self):
-        """ Populates both regular and SSA variable lists. """
+        """ Populates the regular variable list. """
         self.variableTable.setRowCount(0)
-        self.ssaVariableTable.setRowCount(0)
         
         if not self.bv:
             return
@@ -67,14 +67,9 @@ class VariableListWidget(SidebarWidget):
 
         # Populate normal variables
         for var in func.vars:
-            self.addVariableToTable(self.variableTable, var.name, self.get_variable_value(func, var))
+            self.addVariableToTable(self.variableTable, var.name, self.get_variable_value(func, var), self.get_variable_location(var))
 
-        # Populate SSA variables
-        if func.ssa_form:
-            for ssa_var in func.ssa_form.vars:
-                self.addVariableToTable(self.ssaVariableTable, str(ssa_var), self.get_variable_value(func, ssa_var))
-
-    def addVariableToTable(self, table, name, value):
+    def addVariableToTable(self, table, name, value, location):
         """ Adds a variable to the specified table. """
         row_position = table.rowCount()
         table.insertRow(row_position)
@@ -87,15 +82,64 @@ class VariableListWidget(SidebarWidget):
         value_item.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
         table.setItem(row_position, 1, value_item)
 
+        location_item = QTableWidgetItem(location)
+        location_item.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
+        table.setItem(row_position, 2, location_item)
+
     def get_variable_value(self, func, var):
         """ Placeholder for getting variable values. """
         return "N/A"  # Modify this if you can extract values from BN
+
+    def get_variable_location(self, var):
+        """ Returns the memory address or register name of the variable. """
+        if var.storage:
+            return str(var.storage)
+        return "Unknown"
 
     def notifyViewLocationChanged(self, view, location):
         """ Updates the variable lists when the cursor moves. """
         if location:
             self.current_offset = location.getOffset()
             self.populateVariableList()
+
+    def notifyVariableRenamed(self, var, name):
+        """ Updates the table when a variable is renamed. """
+        self.populateVariableList()
+    
+    def function_updated(self, view, func):
+        """ Called when a function is updated. """
+        self.populateVariableList()
+    
+    def function_update_requested(self, view, func):
+        """ Called when a function update is requested. """
+        self.populateVariableList()
+    
+    def undo_entry_added(self, view, entry):
+        """ Called when an undo entry is added, which may include variable renaming. """
+        self.populateVariableList()
+
+    def highlight_variable_usage(self):
+        """ Highlights lines where the selected variable is used. """
+        selected_items = self.variableTable.selectedItems()
+        if not selected_items:
+            return
+        
+        variable_name = selected_items[0].text()
+        
+        func_list = self.bv.get_functions_containing(self.current_offset)
+        if not func_list:
+            return
+        
+        func = func_list[0]
+        for block in func.low_level_il:
+            for instr in block:
+                # Check if the instruction uses the variable by checking if the variable name is in the instruction
+                if variable_name in str(instr):
+                    # Highlight the instruction in green
+                    blocks = self.bv.get_basic_blocks_at(instr.address)
+                    for block in blocks:
+                        block.set_auto_highlight(HighlightColor(HighlightStandardColor.GreenHighlightColor, alpha=128))
+                        block.function.set_auto_instr_highlight(instr.address, HighlightStandardColor.GreenHighlightColor)
 
 class VariableListWidgetType(SidebarWidgetType):
     name = "Variable List"
