52574fa208aef34e8703e9cd4c2ef8423c9f936a
Louie Orcinolo <liatmp3@gmail.com>
2025-02-20 20:54:48 -0500

    UI code, less shit

--- ui/variable_list.py
+++ ui/variable_list.py
@@ -1,163 +1,164 @@
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QImage
-from binaryninja import BinaryView, PluginCommand, HighlightStandardColor, HighlightColor
-from binaryninjaui import *
 from PySide6.QtWidgets import (
     QVBoxLayout, QTableWidget, QTableWidgetItem,
-    QHeaderView, QTabWidget, QWidget
+    QHeaderView, QTabWidget, QWidget, QLineEdit
 )
+from binaryninja import BinaryView, PluginCommand, HighlightStandardColor
+from binaryninjaui import SidebarWidget, SidebarWidgetType, SidebarWidgetLocation
 
 class VariableListWidget(SidebarWidget):
-    def __init__(self, name, frame, data):
+    def __init__(self, name, frame, bv: BinaryView):
         super().__init__(name)
-        self.bv = data
+        self.bv = bv
         self.current_offset = 0
+        self.previous_highlighted = []
+        self._setup_ui()
+        if bv:
+            self._initialize_data()
+    
+    def _setup_ui(self):
+        self.layout = QVBoxLayout(self)
         
-        self.layout = QVBoxLayout()
+        # Add a search bar
+        self.search_bar = QLineEdit()
+        self.search_bar.setPlaceholderText("Search Variables...")
+        self.search_bar.textChanged.connect(self.filter_variables)
+        self.layout.addWidget(self.search_bar)
+        
+        # Create tabs and tables
         self.tabWidget = QTabWidget()
-
-        # Regular variable table
-        self.variableTable = self.createTable()
-
-        # Wrap in QWidget for tab layout
-        self.variableTab = QWidget()
+        self.variableTable = self._create_table()
+        
+        self.variableTab = self._wrap_in_widget(self.variableTable)
         self.ssaTab = QWidget()
-
-        # Add layout to each tab
-        var_layout = QVBoxLayout()
-        var_layout.addWidget(self.variableTable)
-        self.variableTab.setLayout(var_layout)
-
-        ssa_layout = QVBoxLayout()
-        self.ssaTab.setLayout(ssa_layout)  # Leave SSA tab empty
-
-        # Add tabs
+        
         self.tabWidget.addTab(self.variableTab, "Variables")
         self.tabWidget.addTab(self.ssaTab, "SSA Variables")
-
         self.layout.addWidget(self.tabWidget)
-        self.setLayout(self.layout)
-
-        if self.bv:
-            self.populateVariableList()
-            self.bv.register_notification(self)
-
-        self.variableTable.itemSelectionChanged.connect(self.highlight_variable_usage)
-
-    def createTable(self):
-        """ Creates a QTableWidget with three columns. """
-        table = QTableWidget()
-        table.setColumnCount(3)
+        
+        # Connect the selection change signal
+        self.variableTable.itemSelectionChanged.connect(self.highlight_instructions)
+        self.variableTable.itemDoubleClicked.connect(self.edit_cell)
+    
+    def _initialize_data(self):
+        self._populate_variable_list()
+    
+    def _create_table(self):
+        table = QTableWidget(0, 3)
         table.setHorizontalHeaderLabels(["Variable", "Value", "Location"])
         table.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
         return table
-
-    def populateVariableList(self):
-        """ Populates the regular variable list. """
+    
+    @staticmethod
+    def _wrap_in_widget(widget):
+        container = QWidget()
+        layout = QVBoxLayout(container)
+        layout.addWidget(widget)
+        return container
+    
+    def _populate_variable_list(self):
         self.variableTable.setRowCount(0)
-        
         if not self.bv:
             return
-
-        func_list = self.bv.get_functions_containing(self.current_offset)
-        if not func_list:
+        
+        func = next(iter(self.bv.get_functions_containing(self.current_offset)), None)
+        if not func:
             return
-
-        func = func_list[0]
-
-        # Populate normal variables
+        
         for var in func.vars:
-            self.addVariableToTable(self.variableTable, var.name, self.get_variable_value(func, var), self.get_variable_location(var))
-
-    def addVariableToTable(self, table, name, value, location):
-        """ Adds a variable to the specified table. """
-        row_position = table.rowCount()
-        table.insertRow(row_position)
-
-        name_item = QTableWidgetItem(name)
-        name_item.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
-        table.setItem(row_position, 0, name_item)
-
-        value_item = QTableWidgetItem(value)
-        value_item.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
-        table.setItem(row_position, 1, value_item)
-
-        location_item = QTableWidgetItem(location)
-        location_item.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable)
-        table.setItem(row_position, 2, location_item)
-
-    def get_variable_value(self, func, var):
-        """ Placeholder for getting variable values. """
-        return "N/A"  # Modify this if you can extract values from BN
-
-    def get_variable_location(self, var):
-        """ Returns the memory address or register name of the variable. """
-        if var.storage:
-            return str(var.storage)
-        return "Unknown"
-
+            self._add_variable_to_table(var.name, self._get_variable_value(var), self._get_variable_location(var))
+    
+    def _add_variable_to_table(self, name, value, location):
+        row = self.variableTable.rowCount()
+        self.variableTable.insertRow(row)
+        for col, text in enumerate([name, value, location]):
+            item = QTableWidgetItem(text)
+            item.setFlags(Qt.ItemIsEnabled | Qt.ItemIsSelectable | Qt.ItemIsEditable)
+            self.variableTable.setItem(row, col, item)
+    
+    @staticmethod
+    def _get_variable_value(var):
+        return "N/A"
+    
+    @staticmethod
+    def _get_variable_location(var):
+        return str(var.storage) if var.storage else "Unknown"
+    
     def notifyViewLocationChanged(self, view, location):
-        """ Updates the variable lists when the cursor moves. """
         if location:
             self.current_offset = location.getOffset()
-            self.populateVariableList()
-
+            self._populate_variable_list()
+    
     def notifyVariableRenamed(self, var, name):
-        """ Updates the table when a variable is renamed. """
-        self.populateVariableList()
+        self._populate_variable_list()
     
     def function_updated(self, view, func):
-        """ Called when a function is updated. """
-        self.populateVariableList()
+        self._populate_variable_list()
     
     def function_update_requested(self, view, func):
-        """ Called when a function update is requested. """
-        self.populateVariableList()
+        self._populate_variable_list()
     
     def undo_entry_added(self, view, entry):
-        """ Called when an undo entry is added, which may include variable renaming. """
-        self.populateVariableList()
-
-    def highlight_variable_usage(self):
-        """ Highlights lines where the selected variable is used. """
+        self._populate_variable_list()
+    
+    def filter_variables(self):
+        search_text = self.search_bar.text().lower()
+        
+        for row in range(self.variableTable.rowCount()):
+            variable_name = self.variableTable.item(row, 0).text().lower()
+            if search_text in variable_name:
+                self.variableTable.setRowHidden(row, False)
+            else:
+                self.variableTable.setRowHidden(row, True)
+    
+    def highlight_instructions(self):
         selected_items = self.variableTable.selectedItems()
-        if not selected_items:
+        if not selected_items or not self.bv:
             return
         
-        variable_name = selected_items[0].text()
-        
-        func_list = self.bv.get_functions_containing(self.current_offset)
-        if not func_list:
+        selected_var = selected_items[0].text()
+        func = next(iter(self.bv.get_functions_containing(self.current_offset)), None)
+        if not func:
             return
         
-        func = func_list[0]
-        for block in func.low_level_il:
-            for instr in block:
-                # Check if the instruction uses the variable by checking if the variable name is in the instruction
-                if variable_name in str(instr):
-                    # Highlight the instruction in green
-                    blocks = self.bv.get_basic_blocks_at(instr.address)
-                    for block in blocks:
-                        block.set_auto_highlight(HighlightColor(HighlightStandardColor.GreenHighlightColor, alpha=128))
-                        block.function.set_auto_instr_highlight(instr.address, HighlightStandardColor.GreenHighlightColor)
+        # Remove previous highlights
+        for addr in self.previous_highlighted:
+            func.set_user_instr_highlight(addr, HighlightStandardColor.NoHighlightColor)
+        self.previous_highlighted.clear()
+        
+        # Apply new highlights
+        for block in func.basic_blocks:
+            for instr in block.get_disassembly_text():
+                if any(selected_var in token.text for token in instr.tokens):
+                    func.set_user_instr_highlight(instr.address, HighlightStandardColor.BlueHighlightColor)
+                    self.previous_highlighted.append(instr.address)
+    
+    def edit_cell(self, item): # tis broken
+        if item.column() == 0 and self.bv:
+            old_name = item.text()
+            new_name, ok = QInputDialog.getText(self, "Rename Function", "Enter new function name:", text=old_name)
+            if ok and new_name:
+                func = self.bv.get_function_at(self.current_offset)
+                if func:
+                    func.name = new_name
+                    item.setText(new_name)
 
 class VariableListWidgetType(SidebarWidgetType):
     name = "Variable List"
 
     def __init__(self):
-        image = QImage()
-        super().__init__(image, VariableListWidgetType.name)
-
+        super().__init__(QImage(), self.name)
+    
     def createWidget(self, frame, data):
         return VariableListWidget(self.name, frame, data)
 
-def defaultLocation(self):
-    if hasattr(SidebarWidgetLocation, "RightSidebar"):
-        return SidebarWidgetLocation.RightSidebar
-    return SidebarWidgetLocation.LocalSidebar  # Fallback if RightSidebar doesn't exist
+def defaultLocation():
+    return getattr(SidebarWidgetLocation, "RightSidebar", SidebarWidgetLocation.LocalSidebar)
 
-def contextSensitivity(self):
+def contextSensitivity():
     return SidebarContextSensitivity.SelfManagedSidebarContext
 
+# Register the plugin command
 PluginCommand.register("Show Variable List", "Displays a list of variables and SSA variables", lambda bv: None)
 
